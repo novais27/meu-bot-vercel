ffc283b5de4d53722c99a34a2eb30b1acba452e3
novais27 <freitasnovais27@gmail.com>
2025-09-09 10:33:16 +0000

    feat: Adiciona rota unica com app.all para resolver Cannot GET e 404

--- api/index.js
+++ api/index.js
@@ -3,15 +3,17 @@ const app = express();
 
 app.use(express.json());
 
-// Rota de teste
-app.get('/', (req, res) => {
-  res.status(200).send('Seu bot está online!');
-});
-
-// Rota do webhook (usa POST para receber dados)
-app.post('/webhook', (req, res) => {
-  console.log('Webhook recebido!');
-  res.status(200).send('OK');
+// Rota para o seu bot
+app.all('/api', (req, res) => {
+  if (req.method === 'GET') {
+    res.status(200).send('Seu bot está online!');
+  } else if (req.method === 'POST') {
+    // Sua lógica do bot
+    console.log('Webhook recebido!');
+    res.status(200).send('OK');
+  } else {
+    res.status(405).send('Método não permitido');
+  }
 });
 
 module.exports = app;
